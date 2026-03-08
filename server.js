769fbee22b62d833b874f900f39d66fe635277ed
rbajeecasinoadmin-gitlipdev <rbajeecasinoadmin@gmail.com>
2026-03-09 03:32:53 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
 
+app.use(express.static("public"));
+
+app.get("/api/bet/:amount", (req,res)=>{
+
+    let bet = parseInt(req.params.amount);
+    let result = Math.random();
+
+    if(result > 0.5){
+        res.json({result:"WIN", profit:bet*2});
+    }else{
+        res.json({result:"LOSE", profit:0});
+    }
+
+});
+
+app.listen(3000,()=>{
+    console.log("Casino Server Running http://localhost:3000");
+});
