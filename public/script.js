c579ed023dd1ef2d614bc5db37b0df0508831535
rbajeecasinoadmin-gitlipdev <rbajeecasinoadmin@gmail.com>
2026-03-09 03:33:55 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,14 @@
+function play(){
 
+let bet=document.getElementById("bet").value;
+
+fetch("/api/bet/"+bet)
+.then(res=>res.json())
+.then(data=>{
+
+document.getElementById("result").innerText=
+"Result: "+data.result;
+
+});
+
+}
